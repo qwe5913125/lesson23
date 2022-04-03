34fffb32dab742e23d0a2c7b3f19519815721dfd
ivan <qwe5913125@gmail.com>
2022-04-03 10:30:00 +0300

    first commit in lesson 23

---
+++ tests/test_ex2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+def test_upper():
+    assert 'foo'.upper() == 'FOO'
+    # assert 'foo'.upper() != 'FOO'
+    # assert 'foo'.upper() in 'FOO'
+    # assert 'foo'.upper() not in 'FOO'
